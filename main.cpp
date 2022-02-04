80d441b937478ed229fc8c6cd762f0ecc3a98ad1
Jeffrey Jernstrom <smidt@pdx.edu>
2022-02-03 18:15:52 -0800

    woopsie pt 2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@ using namespace std;
 int main()
 {
   cout << "Capstone Team Brown Hope:" << endl;
-  cout << "Mack Foggia" << endl cout << "Jeff Jernstrom" << endl;
+  cout << "Mack Foggia" << endl;
+  cout << "Jeff Jernstrom" << endl;
 
+  int *x = NULL;
+
+  cout << *x;
   return 0;
 }
