a28ccf6b4415c4bf4967149e450bdd4eaa6bf259
baloveless <baloveless@icloud.com>
2022-03-07 16:41:09 -0800

    Create a home page mockup

--- frontend/brown_hope_aid_front_end/src/App.js
+++ frontend/brown_hope_aid_front_end/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <figure> 
+            <img src="./bh_full-color_stacked_black.png" class="navlogo"/>
+          </figure>
+          <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">PROFILE</a></li>
+          </ul>
+        </nav>
+        <h1>BROWN HOPE AID APP</h1>
       </header>
     </div>
   );
