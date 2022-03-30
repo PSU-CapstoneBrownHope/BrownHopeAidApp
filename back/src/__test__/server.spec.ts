cdb70b63df6fbd606a2ad1ce74f4f01fde8e4069
Austin Britton <aubrit2@pdx.edu>
2022-03-29 23:35:48 -0700

    added test for post route

--- back/src/__test__/server.spec.ts
+++ back/src/__test__/server.spec.ts
@@ -1,6 +1,8 @@
 import appx from '../server'
 import supertest from 'supertest'
 
+
+
 describe("test http server", () => {
     test('checks for 200 status code at main route', async () => {
         await supertest(appx).get('/').expect(200)
@@ -14,4 +16,32 @@ describe("test airtable route", () => {
     })
     appx.close();
 })
+
+/*describe("test airtable route", () => {
+    test('checks for 200 status code at airtable route', async (done) => {
+       supertest(appx).post('/api/airtable')
+        .send({application_id: '42'})
+        .set('Accept','application/json')
+        .expect(200)
+        .end((err)=>{
+            if(err) return done(err)
+            return done
+        })
+    })
+    appx.close();
+})
+*/
+
+describe('post endpoint for airtable route', () => { 
+    it('should get the data for this id', async () => {
+        const res = await supertest(appx)
+        .post('/api/airtable/application_status')
+        .send({
+            application_id: '42'
+        })
+        expect(res.statusCode).toEqual(201)
+      
+
+    })
+ })
 
--- back/src/routes/airtable.ts
+++ back/src/routes/airtable.ts
@@ -6,4 +6,8 @@ airtableRouter.get('/', (req, res, next) => {
     res.sendStatus(200)
 })
 
+airtableRouter.post('/application_status', (req, res, next) => {
+    res.sendStatus(201)
+})
+
 export default airtableRouter;
