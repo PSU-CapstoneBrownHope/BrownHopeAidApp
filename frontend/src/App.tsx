091d29afa484ed5612e8238f8c82f10edd137ed2
baloveless <baloveless@icloud.com>
2022-03-30 21:42:25 -0700

    Added basic framework for routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+
+/*  App contains the nav header and the routes for new pages
+
+    To add a new page:
+      - create new Route in Routes
+      - create new li in ul if you want to add the page to nav
+*/
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <nav>
-          <figure> 
-            <img src="./bh_full-color_stacked_black.png" className="navlogo" alt="bh_logo"/>
+          <figure>
+            <img src="./bh_full-color_stacked_black.png" className="navlogo" alt="bh_logo" />
           </figure>
           <ul>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">PROFILE</a></li>
+            <li><a href="/">HOME</a></li>
+            <li><a href="profile">PROFILE</a></li>
           </ul>
         </nav>
-        <h1>BROWN HOPE AID APP</h1>
       </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
     </div>
+  );
+}
+
+// I intend for this to be the page that 
+function Home() {
+  return (
+    <>
+      <h1>BROWN HOPE AID APP</h1>
+    </>
+  );
+}
+
+
+function Profile() {
+  return (
+    <>
+      <h1>Profile page</h1>
+    </>
   );
 }
 
