014ac2c9b78ce6f88830766b71b1647c98b0f9a6
Austin Britton <aubrit2@pdx.edu>
2022-03-31 20:41:47 -0700

    remade changes

--- back/src/routes/airtable.ts
+++ back/src/routes/airtable.ts
@@ -1,4 +1,11 @@
+import airtable from 'airtable';
 import Router from 'express';
+require('dotenv').config();
+
+const airtableapikey = process.env.AIR_TABLE_KEY;
+const baseid = process.env.BASE_ID;
+
+const base = new airtable({apiKey: airtableapikey}).base(baseid);
 
 const airtableRouter = Router();
 
@@ -7,6 +14,22 @@ airtableRouter.get('/', (req, res, next) => {
 })
 
 airtableRouter.post('/application_status', (req, res, next) => {
+    const passphrase = req.body.human_readable_passphrase;
+    base('Human Readable Passphrases').select({
+        maxRecords: 1,
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage){
+        records.forEach(function(record){
+            if(record.get('Passphrase') == passphrase){
+                console.log(record.get('FR Record ID'))
+            }
+        })
+
+        fetchNextPage();
+
+    }, function done(err){
+        if(err) {console.log(err); return}
+    });
     res.sendStatus(201)
 })
 
--- back/src/server.ts
+++ back/src/server.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const port = 3000;
 const app = express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.sendStatus(200)
