0f729947625f3b822838a7e85f96526406175796
baloveless <baloveless@icloud.com>
2022-04-09 16:25:34 -0700

    polished look and size of a few things

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 export const Nav = (): JSX.Element =>  {
   return (
       <header className="appHeader">
-      <nav>
+      <nav aria-label="nav">
         <figure>
           <img src="./bh_full-color_stacked_black.png" className="navlogo" alt="Brown Hope Logo" />
         </figure>
