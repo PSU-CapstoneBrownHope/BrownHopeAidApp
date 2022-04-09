f33f06ebbbcf718dfebce36d7acdbefd8cb4617c
baloveless <baloveless@icloud.com>
2022-04-09 14:13:40 -0700

    Fixes to testing. Application status page I thought I had fixed testing but it was causing me issues earlier so I have implemented what I believe is probably the last fix. To avoid issues with warnings in the testing suite use "npm run test:silent."
    Created application status page and
    used the route to "getAllApplications"
    but this will likely have to be changed
    in the future.
    
    there is a new format for the tests to
    copy from App.text.tsx so be sure to
    keep your tests inline with that.

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -4,11 +4,11 @@ export const Nav = (): JSX.Element =>  {
       <header className="appHeader">
       <nav>
         <figure>
-          <img src="./bh_full-color_stacked_black.png" className="navlogo" alt="bh_logo" />
+          <img src="./bh_full-color_stacked_black.png" className="navlogo" alt="Brown Hope Logo" />
         </figure>
         <ul>
-          <li><a href="/">HOME</a></li>
-          <li><a href="profile">PROFILE</a></li>
+          <li><a aria-label="Home" href="/">HOME</a></li>
+          <li><a aria-label="Profile" href="profile">PROFILE</a></li>
         </ul>
       </nav>
       </header>
