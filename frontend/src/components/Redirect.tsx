33ad2abbc6ce54e14db8605fa25029fa153102dc
baloveless <baloveless@icloud.com>
2022-04-15 12:30:03 -0700

    fixed redirect page. Need to make changes to profile page edit before MVP. Might ask for help on that cause it isn't too crazy.

--- frontend/src/components/Redirect.tsx
+++ frontend/src/components/Redirect.tsx
@@ -1,12 +1,13 @@
-import React, { useState, SyntheticEvent } from "react";
+import React, { useState, SyntheticEvent, useEffect } from "react";
 import styles from "../styles/Buttons.module.css"
 import axios from "axios";
 import { routes } from "../util/config";
-import { useNavigate, Link } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 
 export const Redirect = (): JSX.Element => {
+  const { id } = useParams()
 
   return (
-    <div/>
+    <div>ID: {id}</div>
   )
 }
