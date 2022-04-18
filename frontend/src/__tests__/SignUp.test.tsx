7e06886bb303e1bf2c24a00c9ac74c1e53a0b5d3
Adrian Perez Gonzalez <adrian27@pdx.edu>
2022-04-17 17:49:22 -0700

    SignUp Unit Test

--- frontend/src/__tests__/SignUp.test.tsx
+++ frontend/src/__tests__/SignUp.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {screen} from "@testing-library/react";
+import {fireEvent, screen} from "@testing-library/react";
 import * as ReactDOMClient from 'react-dom/client';
 import {act} from "react-dom/test-utils"
 import { BrowserRouter } from 'react-router-dom';
@@ -28,5 +28,33 @@ test('All signup elements render', async() => {
   expect(screen.getByText("Back to Login")).toBeInTheDocument
   expect(screen.getByText("Create Your Account")).toBeInTheDocument
 });
+
 // TODO:
 // check input field values change on user input
+// This has been done now! ~ Adrian Perez Gonzalez
+
+test('Sing-Up Input Fields Test', async() => {
+  act(() => {
+    ReactDOMClient.createRoot(container).render(<BrowserRouter><SignUp /></BrowserRouter>);
+  });
+
+  const email = screen.getByPlaceholderText("email");
+  fireEvent.change(email, { target: { value: "foo@pdx.edu" } });
+  // weird error with value, exists but doesn't think it does
+  expect(email.value).toBe("foo@pdx.edu") 
+
+  const username = screen.getByPlaceholderText("username");
+  fireEvent.change(username, { target: { value: "foo_bar" } });
+  // weird error with value, exists but doesn't think it does
+  expect(username.value).toBe("foo_bar") 
+
+  const password = screen.getByPlaceholderText("password");
+  fireEvent.change(password, { target: { value: "Password123!@#" } });
+  // weird error with value, exists but doesn't think it does
+  expect(password.value).toBe("Password123!@#") 
+
+  const verify_password = screen.getByPlaceholderText("confirm password");
+  fireEvent.change(verify_password, { target: { value: "Password123!@#" } });
+  // weird error with value, exists but doesn't think it does
+  expect(password.value).toBe("Password123!@#") 
+});
