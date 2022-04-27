68bae814ba8c746d794174cf61758feeff9442b4
baloveless <baloveless@icloud.com>
2022-04-27 16:41:13 -0700

    Incorporates many of the changes suggested. I'm thinking about adding a drop down to the nav that would condense some of the things added. Also found a tranparent logo for the tab icon

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,7 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import {Link} from "react-router-dom"
+import { routes } from "../util/config";
+import axios from 'axios';
 export const Nav = (): JSX.Element => {
+
  
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState(sessionStorage.getItem('username'));
+  const [link, setLink] = useState("login");
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  function logout() {
+    const sendLogoutRequest = async () => {
+      try {
+        const resp = await axios.post(routes.signout, { withCredentials: true });
+        console.log(resp.data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    sendLogoutRequest()
+    window.sessionStorage.clear();
+    setLoggedIn(false)
+    window.location.reload()
+  }
+
+  function Logout() {
+    return (
+      <li onClick={logout}><a href="/" aria-label="Logout">Logout</a></li>
+    );
+  }
 
   useEffect(() => {
     getUsername()
@@ -9,21 +36,29 @@ export const Nav = (): JSX.Element => {
 
   function getUsername() {
     const ssUsername = sessionStorage.getItem('username');
-    if (ssUsername === "" || ssUsername === null)
-      setUsername("PROFILE");
-    else 
+    if (ssUsername === "" || ssUsername === null) {
+      setUsername("LOGIN");
+      setLink("login")
+      setLoggedIn(false)
+    } else {
       setUsername(ssUsername);
+      setLink("profile")
+      setLoggedIn(true)
+    }
   }
 
   return (
       <header className="appHeader">
       <nav aria-label="nav">
+        <a href="https://www.brownhope.org/">
         <figure>
           <img src="./bh_full-color_stacked_black.png" className="navlogo" alt="Brown Hope Logo" />
-        </figure>
+          </figure>
+        </a>
         <ul>
           <li><a aria-label="Home" href="/">HOME</a></li>
-          <li><a aria-label="Profile" href="profile"> {username} </a></li>
+          <li><a aria-label="Profile" href={link}> {username} </a></li>
+          {loggedIn ? <Logout/> : <li hidden></li>} 
         </ul>
       </nav>
       </header>
