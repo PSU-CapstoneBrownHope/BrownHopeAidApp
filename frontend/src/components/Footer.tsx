9b0a960b56939ffbca9a3f4934b5bbc76f11056b
adrian27-pdx <adrian27@pdx.edu>
2022-04-28 20:08:58 -0700

    "Footer Styles"

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,14 @@
+import { info } from 'console';
 import React from 'react';
 
 export const Footer = (): JSX.Element =>  {
+  const infoEmail = "info@blackresiliencefund.com"
+  const contactPage = "https://www.brownhope.org/contact"
   return (
     <div className="footer">
-      Having a hard time using the site? Contact Us At: https://www.brownhope.org/contact
+      <p>Having a hard time using the site? </p>
+      <p>Contact us at: {infoEmail}</p>
+      <p>Or for other forms of contact: {contactPage}</p>
     </div>
   )
 }
