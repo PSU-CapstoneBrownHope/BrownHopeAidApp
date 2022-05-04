fc37d9fd160481a2215d6b12ed0541793255a928
baloveless <baloveless@icloud.com>
2022-05-03 18:40:18 -0700

    Fixed weird logo location

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -50,7 +50,7 @@ export const Nav = (): JSX.Element => {
   return (
       <header className="appHeader">
       <nav aria-label="nav">
-        <a href="https://www.brownhope.org/">
+        <a href="/" className="logo">
         <figure>
           <img src="bh_full-color_stacked_black.png" className="navlogo" alt="Brown Hope Logo" />
           </figure>
