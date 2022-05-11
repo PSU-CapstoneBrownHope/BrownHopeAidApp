df08a28e936bf9f580c3fbc88a47c5280ab32129
baloveless <baloveless@icloud.com>
2022-05-10 20:17:02 -0700

    Verification Page set up need to check in with backend progress to know when this is ready to be put into a PR. It is set up to the specifications discussed in the meeting on 5/10.

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -7,11 +7,12 @@ import text from "../styles/Text.module.css"
 import { LoginCheck } from "../util/userFunctions";
 
 export const SignUp = (): JSX.Element => {
-  const { id } = useParams()
   const [email, setEmail] = useState("");
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [verifyPassword, setVerifyPassword] = useState("");
+  const [pin, setPin] = useState("");
+  const [verificationScreen, setVerificationScreen] = useState(false);
 
   const navigate = useNavigate();
 
@@ -27,105 +28,165 @@ export const SignUp = (): JSX.Element => {
     }
   }
 
+  function handleVerificationSubmit(event: SyntheticEvent) {
+    event.preventDefault()
+    // sends request to get email verification to backend
+    const sendVerificationRequest = async () => {
+      try {
+        const newVerificationRequest = {
+          email: email
+        }
+        const resp = await axios.post(routes.email, newVerificationRequest);
+        setVerificationScreen(true)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    sendVerificationRequest();
+  }
 
 
   // Was thinking the id should be added to this call
   function handleSignupSubmit(event: SyntheticEvent) {
     event.preventDefault()
-    const newSignupRequest = {
-      username: username,
-      email: email,
-      password: password,
-      id: id,
-    };
-
-    const newLoginRequest = {
-      username: username,
-      password: password,
-    };
-
     const sendSignupRequest = async () => {
       try {
+        const newSignupRequest = {
+          username: username,
+          email: email,
+          password: password,
+          // double check name with mack before sending PR
+          pin: pin
+        };
+
+        const newLoginRequest = {
+          username: username,
+          password: password,
+        };
         const resp = await axios.post(routes.signup, newSignupRequest);
-        if (resp.data === "Success") {
-          window.sessionStorage.setItem("username", username)
-
-          const loginResp = await axios.post(routes.login, newLoginRequest, {
-            withCredentials: true,
-          });
-
-          if (loginResp.data === "Success") {
-            alert("Account Creation Successful!")
+        switch (resp.data) {
+          case "Success":
+            const loginResp = await axios.post(routes.login, newLoginRequest, {
+              withCredentials: true,
+            });
+            if (loginResp.data === "Success") {
+              window.sessionStorage.setItem("username", username);
+              alert("Account Creation Successful!")
+              window.location.reload()
+            } else {
+              navigate("login")
+            }
+            break;
+          case "Email Already Exists":
+            alert("Sorry, user already exists")
+            break;
+          case "Emailed":
+            alert("This email already exists in our system. To claim your account, please follow the link emailed to you.")
             window.location.reload()
-          }
-        } else if (resp.data === "Email Already Exists") {
-          alert("Sorry, user already exists")
-        }
-        else if (resp.data === "Emailed") {
-          alert("This email already exists in our system. To claim your account, please follow the link emailed to you.")
+            break;
         }
       } catch (err) {
         alert("Sign up failed");
-        // Handle Error Here
+        navigate("/")
         console.error(err);
       }
     };
     sendSignupRequest();
   }
 
+  const VerificationForm = () => {
+    return (
+      <form id="signUp" className="info" onSubmit={handleSignupSubmit}>
+        <p className={text["medium"]}>
+          Please enter the pin sent to {email}
+        </p>
+        <label htmlFor="pin" className={text["wrapper"]}>
+            <input
+              name="pin"
+              id="pin"
+              placeholder='pin'
+              onChange={(e) => setPin(e.target.value)}
+              className={text['textField']}
+              required
+            />
+        </label>
+      </form>
+    )
+  }
+
+  const SignUpForm = () => {
+    return (
+        <form id="signUp" className="info" onSubmit={handleSignupSubmit}>
+          <label htmlFor="email" className={text["wrapper"]}>
+            Email:
+            <input
+              name="email"
+              id="email"
+              placeholder='email'
+              onChange={(e) => setEmail(e.target.value)}
+              className={text['textField']}
+              required
+            />
+          </label>
+          <label htmlFor="username" className={text["wrapper"]}>
+            Username:
+            <input
+              name="username"
+              id="username"
+              placeholder='username'
+              onChange={(e) => setUserName(e.target.value)}
+              className={text['textField']}
+              required
+            />
+          </label>
+          <label htmlFor="password" className={text["wrapper"]}>
+            Password:
+            <input
+              name="password"
+              id="password"
+              type="password"
+              placeholder='password'
+              onChange={(e) => setPassword(e.target.value)}
+              className={text['textField']}
+              required
+            />
+          </label>
+          <label htmlFor="verifyPassword" className={text["wrapper"]}>
+            Confirm Password:
+            <input
+              name="verifyPassword"
+              id="verifyPassword"
+              placeholder='confirm password'
+              type="password"
+              onChange={(e) => setVerifyPassword(e.target.value)}
+              className={text['textField']}
+              required
+            />
+          </label>
+        </form>
+    );
+  }
+
   return (
     <div className="currentPage">
-      <h1>Create Your Account</h1>
-      <div id="signUp" className="info" onSubmit={handleSignupSubmit}>
-        <label htmlFor="email" className={text["wrapper"]}>
-          Email:
-          <input
-            name="email"
-            id="email"
-            placeholder='email'
-            onChange={(e) => setEmail(e.target.value)}
-            className={text['textField']}
-            required
-          />
-        </label>
-        <label htmlFor="username" className={text["wrapper"]}>
-          Username:
-          <input
-            name="username"
-            id="username"
-            placeholder='username'
-            onChange={(e) => setUserName(e.target.value)}
-            className={text['textField']}
-            required
-          />
-        </label>
-        <label htmlFor="password" className={text["wrapper"]}>
-          Password:
-          <input
-            name="password"
-            id="password"
-            type="password"
-            placeholder='password'
-            onChange={(e) => setPassword(e.target.value)}
-            className={text['textField']}
-            required
-          />
-        </label>
-        <label htmlFor="verifyPassword" className={text["wrapper"]}>
-          Confirm Password:
-          <input
-            name="verifyPassword"
-            id="verifyPassword"
-            placeholder='confirm password'
-            type="password"
-            onChange={(e) => setVerifyPassword(e.target.value)}
-            className={text['textField']}
-            required
-          />
-        </label>
-      </div>
+      <h1 hidden={verificationScreen? true: false}>Create Your Account</h1>
+      <h1 hidden={verificationScreen ? false : true}>Enter Verification Code</h1>
+      {verificationScreen? <SignUpForm/>: <VerificationForm/>}
       <div className="buttons">
-        <button className={styles['fullscreenButton'] + " btn btn-success"} onClick={handleSignupSubmit}>Create Account</button>
+        <button
+          className={styles['fullscreenButton'] + " btn btn-success"}
+          onClick={handleVerificationSubmit}
+          hidden={verificationScreen? true: false}
+        >
+          Create Account
+        </button>
+        <button
+          className={styles['fullscreenButton'] + " btn btn-success"}
+          onClick={handleSignupSubmit}
+          hidden={verificationScreen? false: true}
+        >
+          Create Account
+        </button>
         <p className={text["high"]}>
           Leaving this page will NOT affect your application
         </p>
