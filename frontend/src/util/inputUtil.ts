9d3b09441ec242622471bd302f484e50cae42f3b
baloveless <baloveless@icloud.com>
2022-05-19 11:48:34 -0700

    Almost complete Input verification needs to be tweaked. Date always fails.

--- frontend/src/util/inputUtil.ts
+++ frontend/src/util/inputUtil.ts
@@ -1,13 +1,23 @@
+import { formatDate, isValidDate } from "./appStatusConfig";
+
 /**
  * This file contains the utilities needed by forms
  */
+
 export interface IFields {
   label: string, 
   id: string,
-  type?: string,
   value: any,
+  placeholder?: string,
+  type?: string,
+  format?: string
 }
 
+/**
+ * Interface for buttons on page
+ * Please refer to https://getbootstrap.com/docs/4.0/components/buttons/
+ * to change the bootstrapClass property correctly
+ */
 export interface IButtons {
   text: string, 
   type?: string, 
@@ -18,12 +28,18 @@ export interface IButtons {
 
 export const updateField = (e: React.BaseSyntheticEvent, index: number, form: any) => {
   let elementValue = (e.target as HTMLInputElement).value;
+  // special date format
+  if (form[index].format === "date")
+    elementValue = formatDate(elementValue);
   const formCopy: any = [...form];
   formCopy[index].value = elementValue;
   return formCopy;
 }
 
 export function submitVerify(form: IFields[]) {
-  return form.every((item: any) => item.value.length > 0)
+  return form.every((item: any) => {
+    (item.value.length > 0 && item.format !== "date") // not empty
+      || isValidDate(item.value) // valid date format 
+  }) 
 }
 
