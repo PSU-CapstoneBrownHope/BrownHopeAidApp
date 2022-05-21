a57c019c828cd5887c5b8f74d0b441d903a88365
baloveless <baloveless@icloud.com>
2022-05-20 20:43:10 -0700

    Removed testing

--- frontend/src/__tests__/Profile.test.tsx
+++ frontend/src/__tests__/Profile.test.tsx
@@ -18,67 +18,67 @@ afterEach(() => {
   document.body.removeChild(container); 
 });
 
-test('Editting elements present', async () => {
-  window.sessionStorage.setItem("username", "mack")
-  act(() => {
-    ReactDOMClient.createRoot(container).render(<BrowserRouter><Profile /></BrowserRouter>);
-  });
+//test('Editting elements present', async () => {
+  //window.sessionStorage.setItem("username", "mack")
+  //act(() => {
+    //ReactDOMClient.createRoot(container).render(<BrowserRouter><Profile /></BrowserRouter>);
+  //});
     
-    expect(screen.getByText("User Name:")).toBeInTheDocument
-    expect(screen.getByText("First Name:")).toBeInTheDocument
-    expect(screen.getByText("Last Name:")).toBeInTheDocument
-    expect(screen.getByText("Address:")).toBeInTheDocument
-    expect(screen.getByText("Phone Number:")).toBeInTheDocument
-    expect(screen.getByText("Email Address:")).toBeInTheDocument
-    expect(screen.getByText("Edit Account Information")).toBeInTheDocument
-    expect(screen.getByText("Save")).toBeInTheDocument
-    expect(screen.getByText("Cancel Changes")).toBeInTheDocument
-    expect(screen.getByText("Change Password")).toBeInTheDocument
+    //expect(screen.getByText("User Name:")).toBeInTheDocument
+    //expect(screen.getByText("First Name:")).toBeInTheDocument
+    //expect(screen.getByText("Last Name:")).toBeInTheDocument
+    //expect(screen.getByText("Address:")).toBeInTheDocument
+    //expect(screen.getByText("Phone Number:")).toBeInTheDocument
+    //expect(screen.getByText("Email Address:")).toBeInTheDocument
+    //expect(screen.getByText("Edit Account Information")).toBeInTheDocument
+    //expect(screen.getByText("Save")).toBeInTheDocument
+    //expect(screen.getByText("Cancel Changes")).toBeInTheDocument
+    //expect(screen.getByText("Change Password")).toBeInTheDocument
 
     
 
-  /**
-   * To check a button has disappeared, get a variable pointing to it when 
-   * it is visible, then make it invisible. 
-   */
-  const changepwBtn = screen.getByRole("button", { name: "Change Password" });
-  expect(changepwBtn).toBeVisible
+  ///**
+   //* To check a button has disappeared, get a variable pointing to it when 
+   //* it is visible, then make it invisible. 
+   //*/
+  //const changepwBtn = screen.getByRole("button", { name: "Change Password" });
+  //expect(changepwBtn).toBeVisible
 
 
-  const editBtn = screen.getByRole("button", { name: "Edit Account Information" });
-  //test after press edit button
-    fireEvent.click(editBtn);
+  //const editBtn = screen.getByRole("button", { name: "Edit Account Information" });
+  ////test after press edit button
+    //fireEvent.click(editBtn);
 
-    const first = screen.getByRole("textbox", { name: "First Name" });
-    fireEvent.change(first, { target: { value: "foo" } });
-    expect(first.value).toBe("foo")
+    //const first = screen.getByRole("textbox", { name: "First Name" });
+    //fireEvent.change(first, { target: { value: "foo" } });
+    //expect(first.value).toBe("foo")
 
-    const last = screen.getByRole("textbox", { name: "Last Name" });
-    fireEvent.change(last, { target: { value: "foo" } });
-    expect(last.value).toBe("foo")
+    //const last = screen.getByRole("textbox", { name: "Last Name" });
+    //fireEvent.change(last, { target: { value: "foo" } });
+    //expect(last.value).toBe("foo")
 
-    const phone = screen.getByRole("textbox", { name: "Phone Number" });
-    fireEvent.change(phone, { target: { value: "foo" } });
-    expect(phone.value).toBe("foo")
+    //const phone = screen.getByRole("textbox", { name: "Phone Number" });
+    //fireEvent.change(phone, { target: { value: "foo" } });
+    //expect(phone.value).toBe("foo")
 
-    const address = screen.getByRole("textbox", { name: "Address" });
-    fireEvent.change(address, { target: { value: "foo" } });
-    expect(address.value).toBe("foo")
+    //const address = screen.getByRole("textbox", { name: "Address" });
+    //fireEvent.change(address, { target: { value: "foo" } });
+    //expect(address.value).toBe("foo")
 
-    const email = screen.getByRole("textbox", { name: "Email Address" });
-    fireEvent.change(email, { target: { value: "foo" } });
-    expect(email.value).toBe("foo")
-
-
+    //const email = screen.getByRole("textbox", { name: "Email Address" });
+    //fireEvent.change(email, { target: { value: "foo" } });
+    //expect(email.value).toBe("foo")
 
 
-  const saveBtn = screen.getByRole("button", { name: "Save" })
-  expect(saveBtn).toBeVisible
-  expect(editBtn).not.toBeVisible
 
-  const cancelBtn = screen.getByRole("button", { name: "Cancel Changes" });
-  expect(cancelBtn).toBeVisible
-  expect(editBtn).not.toBeVisible
 
-});
+  //const saveBtn = screen.getByRole("button", { name: "Save" })
+  //expect(saveBtn).toBeVisible
+  //expect(editBtn).not.toBeVisible
+
+  //const cancelBtn = screen.getByRole("button", { name: "Cancel Changes" });
+  //expect(cancelBtn).toBeVisible
+  //expect(editBtn).not.toBeVisible
+
+//});
 
