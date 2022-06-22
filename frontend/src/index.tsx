751767566ae7f379c8caf1468978b28323d29667
baloveless <baloveless@icloud.com>
2022-06-22 16:05:50 -0700

    Increased CSS options in config.css * Added btn color choices for danger, success, and secondary. * allows user to change border color and shape around forms.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "../src/styles/App.css"
+import 'bootstrap'
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOMClient from 'react-dom/client'
 
